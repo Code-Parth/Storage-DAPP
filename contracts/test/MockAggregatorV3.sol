b4352147cd7f6e96efa75ec2f5ab190ed30da601
Parth Parmar <84669955+Code-Parth@users.noreply.github.com>
2023-01-25 13:04:27 +0530

    Update MockAggregatorV3.sol

--- contracts/test/MockAggregatorV3.sol
+++ contracts/test/MockAggregatorV3.sol
@@ -1,3 +1,14 @@
+// This is a Solidity smart contract called "MockV3Aggregator" that is used to mock the functionality of a Chainlink aggregator contract. It implements the AggregatorV2V3Interface from the Chainlink library and allows for testing of other contracts that interact with an aggregator contract.
+
+// The contract imports the AggregatorV2V3Interface from the Chainlink library.
+// The contract declares several public variables, including the version of the contract, the number of decimals, the latest answer, the latest timestamp, and the latest round. It also declares two mapping data structures to store answers and timestamps for each round.
+// The contract has a constructor function that takes in the number of decimals and an initial answer as arguments. It sets the decimals variable and calls the "updateAnswer" function to set the initial answer.
+// The contract has an "updateAnswer" function that allows the contract owner to update the latest answer, timestamp, and round. It updates the values of the corresponding variables and stores the answer and timestamp in the mapping data structures.
+// The contract has an "updateRoundData" function that allows the contract owner to update the data for a specific round. It takes in a round ID, answer, timestamp, and startedAt timestamp as arguments.
+// The contract has a "getRoundData" function that allows anyone to view the data for a specific round. It takes in a round ID as an argument and returns the answer, startedAt timestamp, updatedAt timestamp, and round ID.
+// The contract has a "latestRoundData" function that allows anyone to view the data for the latest round. It returns the latest answer, startedAt timestamp, updatedAt timestamp, and round ID.
+// The contract has a "description" function that returns a string describing the contract.
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
